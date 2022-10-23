7b876fcb2b77289623893e14dd1020a4a535bc15
Nate Bass <nw3455@gmail.om>
2022-10-23 04:40:29 -0700

    ### QUICK COMMIT ###

--- learning_blocks_django_server03/settings.py
+++ learning_blocks_django_server03/settings.py
@@ -29,11 +29,11 @@ ALLOWED_HOSTS = [
 ]
 
 CSRF_TRUSTED_ORIGINS = [
-    'http://127.0.0.1:8000', 'http://localhost:8000', 'https://learning-blocks.azurewebsites.net'
+    'http://127.0.0.1:8000', 'http://localhost:8000', 'http://learning-blocks.azurewebsites.net', 'https://www.learning-blocks.azurewebsites.net', 'https://learning-blocks.azurewebsites.net'
 ]
 
 CORS_ORIGIN_WHITELIST = [
-    'http://127.0.0.1:8000',  'http://localhost:8000', 'https://learning-blocks.azurewebsites.net'
+    'http://127.0.0.1:8000',  'http://localhost:8000', 'http://learning-blocks.azurewebsites.net', 'https://www.learning-blocks.azurewebsites.net', 'https://learning-blocks.azurewebsites.net'
 ]
 
 # Application definition
